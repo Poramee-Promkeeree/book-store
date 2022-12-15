d07311f404ee2f5cbbb701674e418f30cdf6d018
psu6510110267 <6510110267@email.psu.ac.th>
2022-12-15 17:37:46 +0700

    เพิ่มส่วนการใช้งานข้อมูล

--- store-server/src/index.ts
+++ store-server/src/index.ts
@@ -1,9 +1,23 @@
 import Koa from 'koa'
+import json from 'koa-json'
+import { koaBody } from 'koa-body'
+import cors from '@koa/cors'
+
+
+import loadFixtures from './fixtures'
 import apiRouter from './api'
+import appConfig from './config'
+
 
 
 const app = new Koa()
 
+app.use(json())
+app.use(cors())
+app.use(koaBody())
+
 app.use(apiRouter.routes())
 
-app.listen(8080)
+app.listen(8000)
+
+loadFixtures(appConfig.clearDataBeforeLodingFixture)
--- store-server/src/simple.ts
+++ store-server/src/simple.ts
@@ -2,7 +2,7 @@ import http from 'http'
 
 const server = http.createServer(function (req, res) {
   res.writeHead(200);
-  res.end('Hello world');
+  res.end('Hello world')
 });
 
-server.listen(8000);
+server.listen(8000)
