c13156764189b23b34691ac08c0ea1e802613b18
psu6510110267 <6510110267@email.psu.ac.th>
2022-12-15 19:40:06 +0700

    สร้าง Categoty Repo และ Mock ข้อมูล

---
+++ store-client/src/repositories/CategoryRepository.ts
@@ -0,0 +1,15 @@
+import Category from "../models/Category";
+import { IRepository } from "./IRepository";
+
+export class CategoryRepository implements IRepository<Category> {
+
+    async getAll(): Promise<Category[] | null>{
+        return [
+            
+            {id : 1,title : 'Fantasy'},
+            {id : 2,title : 'Computer'}
+
+        ]
+      
+    }
+}
---
+++ store-client/src/repositories/index.ts
@@ -0,0 +1,8 @@
+
+import { CategoryRepository } from "./CategoryRepository"
+
+const repositories = {
+  categories: new CategoryRepository(),
+}
+
+export default repositories
