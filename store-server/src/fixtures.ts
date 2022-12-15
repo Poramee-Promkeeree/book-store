2a9fddabdd73051bb25149f9ce221e723f890ca0
psu6510110267 <6510110267@email.psu.ac.th>
2022-12-15 17:26:20 +0700

    สร้าง fixture เพื่อ insert ข้อมูล

---
+++ store-server/src/fixtures.ts
@@ -0,0 +1,17 @@
+import db from "./db"
+
+async function loadFixtures(clearData = false){
+  if(clearData){
+    console.warn('clearing data')
+    await db('category').del()
+  }
+
+  await db.batchInsert('category', [
+    { id: 1, title: 'Fantasy' },
+    { id: 2, title: 'Computer' },
+  ])
+  
+
+}
+
+export default loadFixtures
