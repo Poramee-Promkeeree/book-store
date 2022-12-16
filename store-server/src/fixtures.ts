c524d7dded403e3505c6d51853720f5447dcadd0
psu6510110267 <6510110267@email.psu.ac.th>
2022-12-16 11:32:57 +0700

    เพิ่มการInsertข้อมูลส่วนbook

--- store-server/src/fixtures.ts
+++ store-server/src/fixtures.ts
@@ -3,16 +3,22 @@ import db from "./db"
 async function loadFixtures(clearData = false){
   if(clearData){
     console.warn('clearing data')
+    await db('book').del()
     await db('category').del()
   }
 
   await db.batchInsert('category', [
     { id: 1, title: 'Fantasy' },
     { id: 2, title: 'Computer' },
-    { id: 3, title: 'Doujin' },
+    { id: 3, title: 'Doujin' }
   ])
   
-
+  await db.batchInsert('book', [
+    { id: 1, title: 'Harry Potter', price: 560, stockAmount: 10, categoryId: 1 },
+    { id: 2, title: 'Game of Thrones', price: 520, stockAmount: 6, categoryId: 1 },
+    { id: 3, title: 'Node.js', price: 300, stockAmount: 4, categoryId: 2 },
+    { id: 4, title: 'Onichichi', price: 149, stockAmount: 9, categoryId: 3 },
+  ])
 }
 
 export default loadFixtures
