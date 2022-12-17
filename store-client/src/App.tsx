278390c70943ee10759a10993151ed19eb6249d0
psu6510110267 <6510110267@email.psu.ac.th>
2022-12-17 15:24:37 +0700

    แสดงรายละเอียดหนังสือ

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,22 +1,33 @@
 import Repo from './repositories'
 import Category from './models/Category'
 import { useEffect, useState } from 'react'
+import Book from './models/Book';
+import BookDetail from './components/BookDetail';
 
 
 
 function App() {
   const [categoryList, setCategoryList] = useState<Category[]>([])
+  const [bookList, setBookList] = useState<Book[]>([])
   
-
   const fetchCategoryList = async () => {
     const result = await Repo.categories.getAll()
     if (result) {
       setCategoryList(result)
     }
   }
+  const fetchBookList = async () => {
+    const result = await Repo.books.getAll()
+    if(result){
+      setBookList(result)
+    }
+  }
+  
+  
 
   useEffect(() => {
     fetchCategoryList()
+    fetchBookList()
   })
 
 
@@ -24,7 +35,11 @@ function App() {
       <div>
         <hr /> 
           {categoryList.map(category => <p key={category.id}>  Title : {category.title}</p>)}  
-        <hr />  
+        <hr />
+        <div>
+        {bookList.map(book => <div key={book.id}><BookDetail {...book}/> <hr /> </div> )}
+        </div>
+ 
       </div>
 
   )
